8fa0ae6503513207d5f4d87399aab54165e048c2
Eelsie <elsie@iki.fi>
2017-09-06 15:28:41 +0300

    started google Oauth

--- src/app/createApp.js
+++ src/app/createApp.js
@@ -4,6 +4,9 @@ import express from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import passport from 'passport'
+import {Strategy} from 'passport-google-oauth20'
+
 
 import connectDb from './connectDb'
 import createEndpoints from './createEndpoints'
@@ -16,6 +19,15 @@ export default (): $Application => {
   app.use(helmet())
   app.use(cors())
   app.use(bodyParser.json())
+
+  passport.use(new Strategy({
+    clientID: process.env.googleClientID,
+    clientSecret: process.env.googleSecretClient,
+    callbackURL: '/auth/google/callback',
+  }),
+  (accessToken) => {
+    console.log(accessToken)
+  })
 
   connectDb()
   createEndpoints(app)
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 // @flow
 
 import createPost from '$modules/post/createPost'
+import passport from 'passport'
 import getPost from '$modules/post/getPost'
 import notFound from '$modules/errors/404'
 
 import {auth} from '$modules/post/middlewares'
 
 export default {
+  'get /auth/google': {handler: passport.authenticate('google', {scope: ['profile', 'email']})},
   'post /posts': {handler: createPost, middlewares: [auth]},
   'get /posts/:id': {handler: getPost},
   'get *': {handler: notFound},
