78f3df0c44ad62f42b5b3d260d4d0352cbd3af46
Eelsie <elsie@iki.fi>
2017-09-12 10:19:14 +0300

    Changes to routes

--- src/routes.js
+++ src/routes.js
@@ -3,19 +3,17 @@
 import createPost from '$modules/post/createPost'
 import passport from 'passport'
 import getPost from '$modules/post/getPost'
-import loginUser from '$modules/user/loginUser'
 import notFound from '$modules/errors/404'
 
 import {auth} from '$modules/post/middlewares'
 
 export default {
   'get /auth/google': {handler: passport.authenticate('google', {scope: ['profile', 'email']})},
-  'get /auth/google/callback': {handler: loginUser},
+  'get /auth/google/callback': {handler: (passport.authenticate('google'), (req, res) => {res.redirect('http://localhost:3000/home')})},
   // $FlowFixMe
-  'get /api/current_user': {handler: (req, res) => {res.send(req.user)}},
-  // $FlowFixMe
-  'get /api/logout': {handler: (req, res) => {req.logout(); res.send(req.user)}},
+  'get /api/logout': {handler: (req, res) => {req.logout(); res.redirect('http://localhost:3000')}},
   'post /posts': {handler: createPost, middlewares: [auth]},
   'get /posts/:id': {handler: getPost},
   'get *': {handler: notFound},
+  'get /api/current_user': {handler: ((req, res) => {res.send(req.user)})},
 }
